7ae3a17e11c0abd130567abad93f56bde13d8151
Daniel Bradley <danielbvideo@gmail.com>
2021-10-14 21:40:07 -0400

    added query selector

--- Main/public/js/homepage.js
+++ Main/public/js/homepage.js
@@ -1,16 +1,3 @@
+let dashboardButton = document.querySelector('.dashboardBtn');
+dashboardButton.setAttribute("style", "background-color:#212529");
 
-// const dashboard_Btn = document.getElementById('#dashboardBtn');
-// const myFigsBtn = document.getElementById('#my_figuresBtn');
-// const addFigsBtn = document.getElementById('#add_figuresBtn');
-// const mySetsBtn = document.getElementById('#my_setsBtn');
-// const addSetsBtn = document.getElementById('#add_setsBtn');
-// const researchBtn = document.getElementById('#bricklinkBtn');
-
-
-// function bColorChange() {
-//     if (parent.window.location === "/myFigures") {
-//         console.log("location is here")
-        
-//         document.getElementById("my_figuresBtn").style.backgroundColor = "#212529"
-//     }
-// }
